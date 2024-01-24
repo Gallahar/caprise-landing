753db8449ebfc20e4c6889e698be2709fe2bbeff
Gallahar <david.kushnir.d@gmail.com>
2024-01-24 13:37:45 +0300

    redirect to main after unsubscribe

--- src/app/[locale]/unsubscribe/page.tsx
+++ src/app/[locale]/unsubscribe/page.tsx
@@ -1,23 +1,29 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import s from './unsubscribe.module.scss'
 import { useEffect, useState } from 'react'
 import { unsubscribeUser } from '@/lib/actions/subscribeActions'
 import { useIntl } from 'react-intl'
 
 function UnSubscribePage() {
+	const { push } = useRouter()
 	const [message, setMessage] = useState('')
 	const email = useSearchParams().get('email')
 	const { formatMessage } = useIntl()
 
 	useEffect(() => {
+		let timeoutId: ReturnType<typeof setTimeout>
+
 		const handleUnsubscribe = async () => {
 			if (!email) return
 			const { message } = await unsubscribeUser(email)
 			setMessage(message)
+			timeoutId = setTimeout(() => push('/'), 4000)
 		}
 		handleUnsubscribe()
+
+		return () => clearTimeout(timeoutId)
 	}, [email])
 
 	return (
